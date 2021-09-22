fd2693a828dae82a4463ea3a4543c4ed4d446739
vasudev peshwani <vasudevpeshwani.vp@gmail.com>
2021-09-22 02:15:10 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var btnTrans = document.querySelector("#click_me");
+var txtInput = document.querySelector("#input-txt");
+var output = document.querySelector("#output");
+
+var server = "https://api.funtranslations.com/translate/minion.json";
+
+function translationUrl(text) {
+    return server + "?" + "text=" + text }
+
+function errorhandler (){
+    alert("Server have some issue please try again after sometime")
+}
+
+function translate() {
+    var usertxt = txtInput.value;
+fetch(translationUrl(usertxt))
+    .then ( response => response.json() )
+    .then (json => {
+    var translatedTxt = json.contents.translated;
+    output.innerText = translatedTxt;
+    } )
+    .catch(errorhandler)
+};
+btnTrans.addEventListener("click", translate)
